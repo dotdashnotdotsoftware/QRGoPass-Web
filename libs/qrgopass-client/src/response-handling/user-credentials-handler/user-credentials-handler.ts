6eefa1bea00399b6ffeea83e66c52effee5a53ad
Adam Lewis <dotdashnotdotsoftware@gmail.com>
2025-08-04 18:13:01 +0000

    Restructure user-credentials-handler code to use exit-early approach

--- libs/qrgopass-client/src/response-handling/user-credentials-handler/user-credentials-handler.ts
+++ libs/qrgopass-client/src/response-handling/user-credentials-handler/user-credentials-handler.ts
@@ -2,40 +2,40 @@ import { EncryptionServices } from "../../encryption/encryption-services";
 import { UserCredentials, QRGoPassFailure, FailureReason } from "../../types";
 import { IResponseHandler } from "../i-response-handler";
 
+export const CREDENTIAL_TRANSFER = 1;
+
 export class UserCredentialsHandler implements IResponseHandler {
     constructor(private readonly encryptionServices: EncryptionServices) { }
 
     public async handleResponse(response: any): Promise<UserCredentials | QRGoPassFailure> {
-        const CREDENTIAL_TRANSFER = 1;
 
-        if (CREDENTIAL_TRANSFER == response.V) {
-            const data = response.Data;
-            const userDecryptPromise = this.encryptionServices.decrypt(data.User);
-            const passDecryptPromise = this.encryptionServices.decrypt(data.Pass);
-
-            try {
-                await Promise.all([userDecryptPromise, passDecryptPromise]);
-            } catch (e) {
-                console.log("ERROR: Could not decrypt credentials");
-                return { failureReason: FailureReason.DECRYPTION_FAILURE };
-            }
-
-            const loginInfo = {
-                userIdentifier: await userDecryptPromise,
-                password: await passDecryptPromise
-            } satisfies UserCredentials;
-
-            if (!loginInfo.userIdentifier || !loginInfo.password) {
-                console.log("ERROR: Could not decrypt credentials");
-                return { failureReason: FailureReason.DECRYPTION_FAILURE };
-            }
-            else {
-                console.log("Successfully decrypted credentials");
-                return loginInfo;
-            }
-        } else {
+        if (CREDENTIAL_TRANSFER !== response.V) {
             console.log("Unsuppored right now");
             return { failureReason: FailureReason.UNSUPPORTED_VERSION };
+        }
+
+        const data = response.Data;
+        const userDecryptPromise = this.encryptionServices.decrypt(data.User);
+        const passDecryptPromise = this.encryptionServices.decrypt(data.Pass);
+
+        try {
+            await Promise.all([userDecryptPromise, passDecryptPromise]);
+        } catch (e) {
+            console.log("ERROR: Could not decrypt credentials");
+            return { failureReason: FailureReason.DECRYPTION_FAILURE };
+        }
+
+        const loginInfo = {
+            userIdentifier: await userDecryptPromise,
+            password: await passDecryptPromise
+        } satisfies UserCredentials;
+
+        if (!loginInfo.userIdentifier || !loginInfo.password) {
+            console.log("ERROR: Could not decrypt credentials");
+            return { failureReason: FailureReason.DECRYPTION_FAILURE };
+        } else {
+            console.log("Successfully decrypted credentials");
+            return loginInfo;
         }
     }
 }
