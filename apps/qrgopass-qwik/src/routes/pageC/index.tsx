e7c0a7518a2486f60a6f78ce08908a685b1b1aeb
Adam Lewis <dotdashnotdotsoftware@gmail.com>
2025-08-24 14:21:53 +0000

    Setup scaffold of a qwik app

---
+++ apps/qrgopass-qwik/src/routes/pageC/index.tsx
@@ -0,0 +1,14 @@
+import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+    return (
+        <><nav>
+            <Link href="/pageA">Page A</Link>
+            <Link href="/pageB">Page B</Link>
+            <Link href="/pageC">Page C</Link>
+        </nav>
+            <div>This is Page C</div>
+        </>
+    );
+});
