f908286a3bf49ba4a8b13106726b0d50c969fb8b
Adam Lewis <dotdashnotdotsoftware@gmail.com>
2025-08-24 16:07:05 +0000

    Prep for testing when merged with main site

--- apps/qrgopass-qwik/src/routes/pageB/index.tsx
+++ apps/qrgopass-qwik/src/routes/pageB/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
+import { DocumentHead, Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
     return (
@@ -12,3 +12,13 @@ export default component$(() => {
         </>
     );
 });
+
+export const head: DocumentHead = {
+    title: "Welcome to Qwik",
+    meta: [
+        {
+            name: "description",
+            content: "Qwik site description",
+        },
+    ],
+};
