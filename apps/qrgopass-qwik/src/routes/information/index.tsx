1991df1a220ab315902ea71748f08172c294c834
Adam Lewis <dotdashnotdotsoftware@gmail.com>
2025-08-25 18:56:50 +0000

    Clean up CSS a little for links

--- apps/qrgopass-qwik/src/routes/information/index.tsx
+++ apps/qrgopass-qwik/src/routes/information/index.tsx
@@ -1,12 +1,13 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import { DocumentHead, Link } from '@builder.io/qwik-city';
 import styles from './information.css?inline';
+import stylesModule from './information.module.css';
 
 export default component$(() => {
     useStylesScoped$(styles);
 
     return (
-        <article>
+        <article class={stylesModule["info-article"]}>
             <h1>Information</h1>
             <div>For any extra details, please don't hesitate to contact us on dotdashnotdotsoftware (at) gmail.com</div>
             <ul>
