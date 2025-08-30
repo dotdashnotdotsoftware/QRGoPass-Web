afbf132b221e849249459e7f9e0aac1d4298c099
Adam Lewis <dotdashnotdotsoftware@gmail.com>
2025-08-30 09:42:06 +0000

    Restructure code to make space for timeout component

--- apps/qrgopass-qwik/src/components/landing-qr/landing-qr.tsx
+++ apps/qrgopass-qwik/src/components/landing-qr/landing-qr.tsx
@@ -1,53 +1,7 @@
-import { component$, Signal, useSignal, useStylesScoped$, useVisibleTask$ } from '@builder.io/qwik';
-// @ts-expect-error - no types available for this package
-import QRCode from 'qrcode-esm';
-import { initialise, UserCredentials } from 'qrgopass-client'
-import { CredentialsRXContainer } from '../credentials-received';
-import styles from "./landing-qr.css?inline"
-
-const QRGOPASS_WEB_CRYPTO_CODE = 5;
+import { component$, Signal } from "@builder.io/qwik";
+import { TransferStage } from "./transfer-stage";
+import { CredentialsRXContainer } from "../credentials-received";
 
 export const LandingQr = component$(({ credentials }: { credentials: Signal<CredentialsRXContainer | null> }) => {
-    const qrCodeSvg = useSignal<string>('');
-    const qrCodeTimeout = useSignal<number>(0)
-    useStylesScoped$(styles);
-
-    useVisibleTask$(async () => {
-        const session = await initialise();
-
-        qrCodeTimeout.value = session.timeout;
-        const scanData = { V: QRGOPASS_WEB_CRYPTO_CODE, UUID: session.uuid, Data: { Key: session.base64EncodedPublicKey } };
-        QRCode.toString(JSON.stringify(scanData), { type: 'svg' })
-            .then((svg: string) => {
-                qrCodeSvg.value = svg;
-            })
-            .catch((err: any) => {
-                console.error('Failed to generate QR code:', err);
-            });
-
-        // Disable as needed for manual testing
-        if (false) {
-            // setTimeout(() => {
-            //     credentials.value = new CredentialsRXContainer("result.userIdentifier", "result.password")
-            // }, 2000)
-
-            return
-        }
-
-        const result = await session.getCredentials() as UserCredentials;
-
-        console.log("result", result)
-        if (result.userIdentifier) {
-            credentials.value = new CredentialsRXContainer(result.userIdentifier, result.password)
-        }
-    });
-
-    return <>
-        <h2>Waiting for connection from mobile app...</h2>
-        {qrCodeSvg.value ? (
-            <div style={{ ["--timeout"]: `${qrCodeTimeout.value / 1000}s` }} dangerouslySetInnerHTML={qrCodeSvg.value} />
-        ) : (
-            <p>Generating QR code...</p>
-        )}
-    </>
-});
+    return <TransferStage credentials={credentials} />
+})
