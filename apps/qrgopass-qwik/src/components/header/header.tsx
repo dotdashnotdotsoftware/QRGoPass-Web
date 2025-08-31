5e379c3388e41a3ac732d1d1e51ce5c30bfed3c4
Adam Lewis <dotdashnotdotsoftware@gmail.com>
2025-08-31 11:39:26 +0000

    Fix missing width and height on store button

--- apps/qrgopass-qwik/src/components/header/header.tsx
+++ apps/qrgopass-qwik/src/components/header/header.tsx
@@ -31,7 +31,7 @@ export const Header = component$(() => {
                 <a href="/about.html">What is this?</a>
                 <Link href="/information" class={moduleStyles["nav-link"]}>FAQs</Link>
                 <a href="https://play.google.com/store/apps/details?id=com.qrgopass.qrgopass.live&hl=en-GB">
-                    <img src="/GetItOnGooglePlay_Badge_Web_color_English.png" />
+                    <img src="/GetItOnGooglePlay_Badge_Web_color_English.png" width={108} height={32} />
                 </a>
             </div>
         </header>
